4cb3d4c323a2af394f963a0aae503c55eeabce02
JoharryJC <energycuenta@yahoo.com>
2022-05-07 21:01:38 +0200

    Template - FormModule and Design of the Form

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+
 
 
 @NgModule({
@@ -14,7 +16,8 @@ import { SwitchesComponent } from './switches/switches.component';
     SwitchesComponent
   ],
   imports: [
-    CommonModule,
+    CommonModule, 
+    FormsModule, 
     TemplateRoutingModule
   ]
 })
