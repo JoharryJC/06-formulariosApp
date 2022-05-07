5b48aa37721e0ec4dfaddc461fb5ae00cf96970e
JoharryJC <energycuenta@yahoo.com>
2022-05-07 22:39:33 +0200

    Implementation of ViewChild and "nombreValido"

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,14 +9,33 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  nombreValido(): boolean {
+    //coloque el signo de interrogacion, para que no evalue lo demas si la 
+    //variable controls no se ha terminado de inicializar 
+    return this.miFormulario?.controls['producto']?.invalid && 
+    this.miFormulario?.controls['producto']?.touched
+  }
+
+  guardar() {
+    console.log('Submit hecho...!');
+    console.log(this.miFormulario.value); 
+    //console.log(miFormulario); 
+  }
+
+  /*
   guardar(miFormulario: NgForm) {
     console.log('Submit hecho...!');
     console.log(miFormulario.value); 
     //console.log(miFormulario); 
   }
+  */
+
+
 }
