c0241808dac52574ab28830e3d9da047c0ef0e25
JoharryJC <energycuenta@yahoo.com>
2022-05-08 21:21:46 +0200

    delete elements dinamically

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -11,6 +11,12 @@ export class BasicosComponent implements OnInit {
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  initForm = {
+    producto: 'RTX 4080ti', 
+    precio: 10,
+    existencias: 10
+  }
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,13 +26,25 @@ export class BasicosComponent implements OnInit {
     //coloque el signo de interrogacion, para que no evalue lo demas si la 
     //variable controls no se ha terminado de inicializar 
     return this.miFormulario?.controls['producto']?.invalid && 
-    this.miFormulario?.controls['producto']?.touched
+    this.miFormulario?.controls['producto']?.touched; 
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.touched && 
+    this.miFormulario?.controls['precio']?.value < 0;      
   }
 
   guardar() {
-    console.log('Submit hecho...!');
+    console.log('Submit o Posteo hecho...!');
     console.log(this.miFormulario.value); 
     //console.log(miFormulario); 
+
+    this.miFormulario.resetForm({
+      producto: 'algo', 
+      precio: 0, 
+      existencias: 0 
+    });
+
   }
 
   /*
