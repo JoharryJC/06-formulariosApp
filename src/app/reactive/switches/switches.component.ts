0bb1f5bce7ba0fb5ba726add65bca3c4a2c7383a
JoharryJC <energycuenta@yahoo.com>
2022-05-11 23:18:12 +0200

    Reactive Forms: Switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required ], 
+    notificaciones: [true, Validators.required], 
+    condiciones: [ false, Validators.requiredTrue ]
+  });
 
-  ngOnInit(): void {
+  persona = {
+    genero: 'F', 
+    notificaciones: true
   }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    //no puedo usar la siguiente instruccion, porque 
+    //el campo de termino de usos, no forma parte del 
+    //objeto persona, entonces lanza un error
+    //entonces debemos usar reset  
+    //this.miFormulario.setValue(this.persona); 
+
+    //this.miFormulario.reset(this.persona); 
+    this.miFormulario.reset({
+      ...this.persona, 
+      condiciones: true 
+    });  
+
+    //la siguiente instruccion es para saber cuando 
+    //hay cambios
+    this.miFormulario.valueChanges.subscribe( form => {
+      console.log(form); 
+
+      delete form.condiciones; 
+      this.persona = form;
+      console.log(form);      
+    });
+
+    //otra forma seria:
+    // this.miFormulario.valueChanges.subscribe( ({condiciones, ...rest}) => {
+    //   this.persona = rest; 
+    // });
+
+  }
+  
+  guardar() {
+    const formValue = {...this.miFormulario.value};         
+    console.log(formValue); 
+
+    //si quiero eliminar el objeto condiciones del formValue, 
+    //debo hacer lo siguiente:
+
+    delete formValue.condiciones; 
+
+    console.log(formValue); 
+
+    this.persona = formValue; 
+
+  }
+
 
 }
