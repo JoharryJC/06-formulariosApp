6829504338a98cbf86420d551f0fb1b226ec4271
JoharryJC <energycuenta@yahoo.com>
2022-05-16 22:51:41 +0200

    Validation of Email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -10,14 +10,20 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.fb.group ({
-    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern) ] ]
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern) ] ], 
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)] ], 
   });
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Fernando Herrera', 
+      email: 'test1@test.com' 
+    })
   }
 
   campoNoValido (campo: string) {
@@ -29,7 +35,7 @@ export class RegistroComponent implements OnInit {
     console.log(this.miFormulario.value); 
 
     this.miFormulario.markAllAsTouched(); 
-    
+
   }
 
 }
